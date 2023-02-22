5f5813f980c89cbfb7e785c06d165646ea459008
Serge Rogatch <serge.rogatch@gmail.com>
2023-02-22 17:53:43 +0100

    Faster inference when limiting slow device memory to 0

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import os
 import time
 import readline
+import textwrap
 
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from accelerate import infer_auto_device_map
 
 def main():
     os.environ["HF_ENDPOINT"] = "https://huggingface.co"
     # checkpoint = "bigscience/bloomz"
-    checkpoint = "EleutherAI/gpt-j-6B"
+    checkpoint = "philschmid/gpt-j-6B-fp16-sharded"
+    # checkpoint = "EleutherAI/gpt-j-6B"
 
     tm_start = time.time()
     tokenizer = AutoTokenizer.from_pretrained(checkpoint)
     model = AutoModelForCausalLM.from_pretrained(
-        checkpoint, torch_dtype="auto", device_map="auto", offload_folder="offload")
+        checkpoint, torch_dtype="auto", device_map="auto", offload_folder="offload",
+        max_memory={0: "23GiB", 1: "0GiB", "cpu": "96GiB"})
     tm_end = time.time()
     print(f'Loaded in {tm_end - tm_start} seconds.')
 
@@ -35,7 +39,7 @@ def main():
         tm_end = time.time()
         print(f'Decoded in {tm_end - tm_start} seconds.')
 
-        print(response)
+        print("\n".join(textwrap.wrap(response, width=120)))
 
 
 if __name__ == '__main__':
