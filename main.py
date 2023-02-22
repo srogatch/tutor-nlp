e60444b9bd68f75f14a948cb416f38361fdcf793
Serge Rogatch <serge.rogatch@gmail.com>
2023-02-22 13:12:03 +0100

    Loop input-output

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
 import time
+import readline
+
+import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def main():
@@ -14,24 +17,25 @@ def main():
     tm_end = time.time()
     print(f'Loaded in {tm_end - tm_start} seconds.')
 
-    prompt = "Which investors can I ask for investments in a software reverse architecting project?"
+    while True:
+        prompt = input('Request to LLM: ')
 
-    tm_start = time.time()
-    inputs = tokenizer.encode(prompt, return_tensors="pt").to("cuda")
-    tm_end = time.time()
-    print(f'Encoded in {tm_end - tm_start} seconds.')
+        tm_start = time.time()
+        inputs = tokenizer.encode(prompt, return_tensors="pt").to("cuda")
+        tm_end = time.time()
+        print(f'Encoded in {tm_end - tm_start} seconds.')
 
-    tm_start = time.time()
-    outputs = model.generate(inputs, max_new_tokens=2048, pad_token_id=tokenizer.eos_token_id, repetition_penalty=1.5)
-    tm_end = time.time()
-    print(f'Generated in {tm_end - tm_start} seconds.')
+        tm_start = time.time()
+        outputs = model.generate(inputs, max_new_tokens=2048, pad_token_id=tokenizer.eos_token_id, repetition_penalty=1.5)
+        tm_end = time.time()
+        print(f'Generated in {tm_end - tm_start} seconds.')
 
-    tm_start = time.time()
-    response = tokenizer.decode(outputs[0])
-    tm_end = time.time()
-    print(f'Decoded in {tm_end - tm_start} seconds.')
+        tm_start = time.time()
+        response = tokenizer.decode(outputs[0])
+        tm_end = time.time()
+        print(f'Decoded in {tm_end - tm_start} seconds.')
 
-    print(response)
+        print(response)
 
 
 if __name__ == '__main__':
